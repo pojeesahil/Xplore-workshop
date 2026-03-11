1daf2fdb66e1ee2a87eb34202c0956ea8d9a2e5d
Pradnyadange <pradnyadange07@gmail.com>
2026-03-12 02:25:41 +0530

    Update boss.py

--- Pradnyadange_solutions/test_playground/basics/boss.py
+++ Pradnyadange_solutions/test_playground/basics/boss.py
@@ -1,52 +1,51 @@
 choice = 'y'
 
-while choice =='y' : # make 'Y' valid too
+while choice =='y' or choice =='Y' : # make 'Y' valid too
     try:
         # typecast the below 2 to a list
         
-        numbers =input("Enter the input numbers separated by spaces: ")
-        operators = input("Enter operators between them: ")
+        numbers =list(map(int,input("Enter the input numbers separated by spaces: ")))
+        operators = list(input("Enter operators between them: "))
 
         # check length matching
 
-        if len(numbers) != len(operators): # this seems odd... u might say it's ... off by one
-            print("What u doin fam ? ") # replace wiht better message :)
+        if len(numbers) != len(operators)-1: # this seems odd... u might say it's ... off by one
+            print("Number of operators must be less than number of numbers.") # replace wiht better message :)
             continue
         
-        flag = False # huh this seems inverted
-        for i in range(len(numbers)): # indexing range fix
-            a, b, op = numbers[i-1], numbers[i], operators[i]
+        flag = True # huh this seems inverted
+        for i in range(len(operators)): # indexing range fix
+            a, b, op = numbers[i-1], numbers[i+1], operators[i]
             # correct the ops
             match op:
                 case '+':
                     c = a + b
                 case '-':
-                    c = a * b
-                case '*':
-                    c = a / b
-                case '/':
-                    c = a % b
-                case '%':
                     c = a - b
+                case '*':
+                    c = a * b
+                case '/':
+                    c = a / b
+                case '%':
+                    c = a % b
                 case '//':
-                    c = a ** b
-                case '**':
                     c = a // b
+                case '**':
+                    c = a ** b
                 case _:
                     flag = True
             if not flag:
                 print("Invalid ops vro")
                 break
 
-            numbers[i-1] = c
+            numbers[i+1] = c
         if not flag:
             continue
-        print(f"Output: numbers[-1]")
-    except Exception:
-        print(f"Exception: ...") # print exception
+        print(f"Output: {numbers[-1]}")
+    except Exception as e:
+        print(f"Exception:{e}") # print exception
     finally:
         choice = input("Do you want to continue? [y/n] : ") # always ask before ending
-
-# can you make the code shorter and with improved answer? 
+# can you make the code shorter and with improved answer? yess
 # like handling any basic arithmetic equation (that may have brackets too) ?
-# u might wanna find a special function in python
+# u might wanna find a special function in python the function named eval()
